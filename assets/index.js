2441105e11d84bbac45ed748e12602c49dbf00b1
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-05-13 18:23:25 +0600

    cdn update with some unused clr

--- assets/index.js
+++ assets/index.js
@@ -8,7 +8,7 @@ var vm = new Vue({
         loanAmountVal: '',
         nhtLoanVal: 0,
         nhtLoanIrVal: '',
-        closing_cost:'',
+        closing_cost: '',
         showSummary: false,
         is_nht_applicable: false
     },
@@ -20,8 +20,7 @@ var vm = new Vue({
                 , o = (1 - s) * n / (s * (1 - Math.pow(s, i)));
             return Number(o.toFixed(2))
         },
-
-        formatNumber: function(val){
+        formatNumber: function (val) {
             return parseFloat(Math.round((val) * 100) / 100).toFixed(2);
         }
     },
@@ -30,13 +29,12 @@ var vm = new Vue({
             var prop_cost = this.propertyCostVal,
                 req_depo = 0,
                 add_depo = this.depositBalanceVal,
-                loan = this.loanAmountVal,
                 loan_term = this.repayYearsVal,
                 interest = this.interestRateVal,
                 nht_loan = this.nhtLoanVal,
                 nht_interest = this.nhtLoanIrVal
 
-            if(!this.is_nht_applicable){
+            if (!this.is_nht_applicable) {
                 nht_loan = 0;
                 nht_interest = 0;
             }
@@ -46,10 +44,9 @@ var vm = new Vue({
             this.closing_cost = prop_cost * closing_rate;
             var loan_amount = prop_cost - (req_depo + add_depo) - nht_loan;
             this.loanAmountVal = loan_amount;
-            var cash_consideration = req_depo + loan_amount + add_depo;
             var monthly_payment = this.calculateMonthlyPayments(interest, loan_term, loan_amount);
-            if(this.is_nht_applicable){
-               return monthly_payment += this.calculateMonthlyPayments(nht_interest, loan_term, nht_loan);
+            if (this.is_nht_applicable) {
+                return monthly_payment += this.calculateMonthlyPayments(nht_interest, loan_term, nht_loan);
             }
             return monthly_payment = Math.round(monthly_payment / 10) * 10;
 
